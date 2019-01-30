d7311c68306a0be663c411404d821a0bd96ed754
Stephen Larkin <stephen.larkin.dev@gmail.com>
2019-01-30 11:46:20 +0000

    reduced polynomial func

--- polynomials.py
+++ polynomials.py
@@ -1,17 +1,17 @@
+from functools import reduce
 # Generate a simple polynomial from inputs
 # Input Name => x
 # coefficients => a
 # degree => n
 
 
-def f(x, a, n):
+def f(x, a):
     # a[0] + a[1]*x + a[2]*x^2 + ... + a[n]*x^n
-    result = 0
-    for index, coefficient in enumerate(a):
-        if index <= n:
-            result += coefficient * (x**index)
 
-    return result
+    return reduce(
+        (lambda a, b: a + b),
+        list(map((lambda c: c[1] * (x**c[0])), enumerate(a)))
+    )
 
 
-print(f(2, [2, 0, 4, -1], 3))
+print(f(2, [2, 0, 4, -1]))
